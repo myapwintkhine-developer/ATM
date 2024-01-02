2aaa1993890796842548fcf85101ad64ba0f11fb
myapwintkhine <myapwintkhine@acedatasystems.com>
2024-01-01 20:37:27 -0800

    add files

---
+++ ATMApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ATMApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
